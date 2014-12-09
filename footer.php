25a0616250bd233a07681fd4faf95a5d048c06e0
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 10:55:35 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div id="footer">
+	<p>{action hook=wp_footer}</p>				
+	<!--					
+	Even though your not required to do this, we'd appreciate					
+	a lot if you didn't remove the notice above.										
+	This way we'll get a better ranking on search engines					
+	and you'll spread the FlatPress word all around the world :)										
+	If you really want to remove it, you may want to consider doing at least a small donation.
+	-->				
+	<!--					Don't remove this comment if you use the template, please.				-->
+	<p>
+<a href="{$smarty.const.BLOG_BASEURL}admin.php" ><img src="{$smarty.const.BLOG_BASEURL}/fp-interface/themes/bagus/imgs/tbagus.png" alt="Admin" title="Admin Area"/></a> Powered by <a href="http://www.flatpress.org">Flatpress</a>. Tema <a title="Tema Bagus">Bagus</a> by <a href="http://eggoez.com/" title="eggoez">eggoez</a>. <div id="bottomcurve"><a href="#top" title="Go Top!">∆</a></div>
+	</p>
+</div><!-- end footer -->
+</div><!-- end container -->
+</body>
+</html>			
